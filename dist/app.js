404b4d2c2cea4d11a2386a2c18e3ad444653fdd0
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2023-01-22 15:21:17 +0100

    Create an empty TypeScript project

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+let style = `
+border-radius: 4px;
+padding: 20px;
+font-size: 30px;
+background-color: #2F74C0;
+color: #F7F7F7;
+text-transform: uppercase;
+margin: 20px 0;
+font-weight: bold;
+
+`;
+console.log('%c Hello TypeScript', style);
+console.log('%c Hi this is my simple todo list created using typescript.', 'color: #2F74C0;');
